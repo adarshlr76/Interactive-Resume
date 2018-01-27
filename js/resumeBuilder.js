7699d3897f35a303a5ea48ed45b096f0f3787b32
Adarsh <adarshlr76@gmail.com>
2018-01-27 18:26:54 +0530

    added initial data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,79 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+ var bio = {
+
+ 	"name" : "Adarsh Lakshmansandra Raju",
+ 	"role" : "full stack developer",
+ 	"contacts" : {
+ 		"mobile" : "(+91) 7019197584",
+ 		"email" : "ra50@hw.ac.uk",
+ 		"github" : "https://github.com/adarshlr76",
+ 		"location" : "bangalore"
+ 	},
+ 	"welcomeMessage" : "",
+ 	"skills" : ["python", "c++", "C#", "HTML", "CSS", "MySQL", "Oracle"],
+ 	"biopic" : "",
+ 	"display" : function () {
+
+ 	}
+
+ };
+
+
+var education = {
+	schools : [ {
+		"name" : "BIT",
+		"location" : "Bangalore",
+		"degree" : "BE",
+		"majors" : "EEE",
+		"dates" : "From : 1994 , To : 1998",
+		"url" : " http://www.bit-bangalore.edu.in/ "
+	}
+	],
+	onlineCourses : [ {
+		"title" : "Full stack web developer ",
+		"school" : "Udacity",
+		"dates" : "2018",
+		"url" : "www.udacity.com"
+	}],
+	display : function () {
+
+	}
+};
+
+
+var work = {
+  "jobs" : [{
+	"employer" : " IIHT Ltd ",
+	"Title" : "IT Trainer",
+	"location" : "bangalore",
+	"dates" : "2013-2016",
+	"description" : "Provided training on Java Hadoop"
+	},
+	{
+	"employer" : " IBM ",
+	"Title" : "Software developer",
+	"location" : "bangalore",
+	"dates" : "2010-2012",
+	"description" : "Balanced warehouse tools developer"
+	}
+	]
+
+};
+
+
+
+var projects = {
+  "projects" : [{
+  "title" : "Item Catalog",
+  "dates" : "2018",
+  "description" : "List of items in different categories",
+  "images" : [" ", " "] 
+	},
+	{
+    "title" : "Online Resume",
+    "dates" : "2018",
+    "description" : "jquery, javascript based single page Resume",
+    "images" : [" ", " "] 
+	}
+	]	
+};
